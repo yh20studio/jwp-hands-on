0e6c891ba5ef8b5426c45ffb2fd823c0e813758f
yh20studio(tiki) <yh20studio@gmail.com>
2022-09-18 19:54:23 +0900

    test: reflection test 완료

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +9,14 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Constructor<Junit4Test> declaredConstructor = clazz.getDeclaredConstructor();
+        final Junit4Test junit4Test = declaredConstructor.newInstance();
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            final boolean annotationPresent = method.isAnnotationPresent(MyTest.class);
+            if (annotationPresent) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,7 +16,18 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
-
+        final Set<Class<?>> controllerAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        final Set<Class<?>> serviceAnnotatedWith = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> repositoryAnnotatedWith = reflections.getTypesAnnotatedWith(Repository.class);
+        for (Class<?> clazz : controllerAnnotatedWith) {
+            log.info("Controller {}", clazz.getName());
+        }
+        for (Class<?> clazz : serviceAnnotatedWith) {
+            log.info("Service {}", clazz.getName());
+        }
+        for (Class<?> clazz : repositoryAnnotatedWith) {
+            log.info("Repository {}", clazz.getName());
+        }
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
 }
