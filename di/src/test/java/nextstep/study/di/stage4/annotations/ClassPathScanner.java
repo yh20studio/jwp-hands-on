284404a026239d39019a87f8c204d9d3f223f8fd
yh20studio(tiki) <yh20studio@gmail.com>
2022-09-27 20:55:48 +0900

    test: stage4 구현 완료

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,18 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        final Reflections reflections = new Reflections(packageName);
+        final Set<Class<?>> allClassesInPackage = new HashSet<>();
+        final Set<Class<?>> serviceClass = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> repositoryClass = reflections.getTypesAnnotatedWith(Repository.class);
+        allClassesInPackage.addAll(serviceClass);
+        allClassesInPackage.addAll(repositoryClass);
+        return allClassesInPackage;
     }
 }
