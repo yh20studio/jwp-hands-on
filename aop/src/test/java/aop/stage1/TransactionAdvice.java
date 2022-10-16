3a69f7a5f013210a535ddfe349819b63860c78c5
yh20studio(tiki) <yh20studio@gmail.com>
2022-10-16 20:18:18 +0900

    feat: stage1 구현 완료

--- aop/src/test/java/aop/stage1/TransactionAdvice.java
+++ aop/src/test/java/aop/stage1/TransactionAdvice.java
@@ -1,15 +1,36 @@
 package aop.stage1;
 
+import aop.DataAccessException;
+import java.lang.reflect.InvocationTargetException;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * 어드바이스(advice). 부가기능을 담고 있는 클래스
  */
-public class TransactionAdvice  implements MethodInterceptor {
+public class TransactionAdvice implements MethodInterceptor {
+
+    private final PlatformTransactionManager platformTransactionManager;
+
+    public TransactionAdvice(final PlatformTransactionManager platformTransactionManager) {
+        this.platformTransactionManager = platformTransactionManager;
+    }
 
     @Override
     public Object invoke(final MethodInvocation invocation) throws Throwable {
-        return null;
+        final TransactionStatus transaction = platformTransactionManager.getTransaction(
+                new DefaultTransactionDefinition());
+        try {
+            final Object result = invocation.getMethod()
+                    .invoke(invocation.getThis(), invocation.getArguments());
+            platformTransactionManager.commit(transaction);
+            return result;
+        } catch (InvocationTargetException e) {
+            platformTransactionManager.rollback(transaction);
+            throw new DataAccessException(e);
+        }
     }
 }
